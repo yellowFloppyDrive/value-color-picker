b26db180a8c307b09db07ae450e15d3bbd4de21e
yellowFloppyDrive <yellowfloppydrive@gmail.com>
2024-11-18 00:56:26 +0100

    Fixed B&W filter

--- src/run/createValueColorToggleButton.ts
+++ src/run/createValueColorToggleButton.ts
@@ -2,9 +2,16 @@ import {createElement} from "./createElement.ts";
 
 export function createValueColorToggleButton(): HTMLButtonElement {
     const grayscaleButton = createElement(`<button>Toggle values/colors</button>`);
+
     if (grayscaleButton instanceof HTMLElement) {
         grayscaleButton.onclick = () => {
-            document.body.style.filter = document.body.style.filter ? '' : 'grayscale(100%)';
+            let blendLayer = document.body.querySelector('[data-component="value-color-blend-layer"]');
+            if (blendLayer) {
+                blendLayer.remove();
+            } else {
+                blendLayer = createElement(`<div data-component="value-color-blend-layer" style="pointer-events: none; position: fixed; top: 0; right: 0; bottom: 0; left: 0; mix-blend-mode: color; background: #fff;"></div>`);
+                document.body.append(blendLayer);
+            }
         }
     }
 
